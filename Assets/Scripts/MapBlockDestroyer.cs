93186c423d1dc12305f302501abc9403081c9318
Guilmeer <guicobata@gmail.com>
2021-07-14 00:36:53 -0300

    Rearrange Files

--- Assets/Scripts/MapBlockDestroyer.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MapBlockDestroyer : MonoBehaviour {
-    private void OnTriggerEnter (Collider other) {
-        if (other.CompareTag ("SpawnPoint")) Destroy (other.gameObject);
-    }
-}
